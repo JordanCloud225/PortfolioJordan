ed730d1c19b8cdea4bb4c71d10e3139f197da912
Hazer <zamblejordan0779@gmail.com>
2026-05-02 11:55:20 +0000

    Updates

--- src/components/layout/LoadingSpinner.vue
+++
@@ -1,8 +0,0 @@
-<template>
-    <div class="flex justify-center items-center h-screen">
-        <Icon icon="eos-icons:bubble-loading" class="text-secondary text-9xl" />
-    </div>
-</template>
-<script>
-
-</script>
